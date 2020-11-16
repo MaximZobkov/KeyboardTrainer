ba1f8860ce01ff98193265ac069db7c99218cf29
Godofcs <chesnov.ilya@bk.ru>
2020-11-16 20:27:06 +0300

    firs_normal_trainer

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,8 @@
 message_input.onkeypress = handle;
-var string_s = message_input2.getAttribute('placeholder');
+var string_s = message_input.getAttribute('placeholder');
 var number = 0;
 var error = 0;
 var error_start = 0;
-alert('g');
 
 function handle(e) {
     /*if (e.key == Backspace){
@@ -12,7 +11,12 @@ function handle(e) {
     }*/
     if (e.key === string_s[0]) {
         string_s = string_s.slice(1);
-        message_input2.setAttribute('placeholder', string_s);
+        message_input.setAttribute('placeholder', string_s);
+    }
+    else {
+        if (e.key == "Backspace"){
+            string_s = "" + string_s
+        }
     }
 }
 
